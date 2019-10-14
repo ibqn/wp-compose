37cbcc4c74c702980581c67caa060694e762687c
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2019-10-14 23:22:35 +0200

    split script in prepare and docker-compose

--- docker-compose.bash
+++ docker-compose.bash
@@ -2,16 +2,6 @@
 
 set -e
 
-python3 -m venv './venv'
-
 source './venv/bin/activate'
 
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade setuptools
-
-python3 -m pip install --upgrade --requirement './requirements.txt'
-
 exec docker-compose "$@"
-
-# you might need to install
-# sudo apt install python3-venv
