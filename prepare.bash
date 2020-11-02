0e3ea155ab9f33d029a16ab193152082692e7566
ibqn <evgen.ibqn@gmail.com>
2020-11-03 00:42:49 +0100

    switch to mariadb

--- prepare.bash
+++ prepare.bash
@@ -8,6 +8,7 @@ source './venv/bin/activate'
 
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade wheel
 
 python3 -m pip install --upgrade --requirement './requirements.txt'
 
